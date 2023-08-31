56e247ccd22b9f494726241008495a72712e7f5f
quamarar <122767728+quamarar@users.noreply.github.com>
2023-08-31 20:48:16 +0530

    Update job2.groovy

--- jenkins/jobs/job2.groovy
+++ jenkins/jobs/job2.groovy
@@ -1,1 +1,15 @@
-#
+pipelineJob('seed-job-demo/Pipelines/job2') {
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('quamarar/seed-job-demo')
+                    }
+                }
+            }
+            scriptPath('jenkins/pipelines/job2.Jenkinsfile')
+        }
+    }
+}
