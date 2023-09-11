c63f0c24bebdc0fccd79168238ba6706165e44d9
quamarar <122767728+quamarar@users.noreply.github.com>
2023-09-11 13:45:55 +0530

    Update job1.groovy

--- jenkins/jobs/job1.groovy
+++ jenkins/jobs/job1.groovy
@@ -1,15 +1,35 @@
 pipelineJob('seed-job-demo/Pipelines/job1') {
 
-    definition {
-        cpsScm {
-            scm {
+multibranchPipelineJob('seed-job-demo/Pipelines/job1') {
+
+    branchSources {
+        branchSource {
+            source {
                 git {
-                    remote {
-                        github('quamarar/seed-job-demo')
+                     id('job1')
+                    remote ('https://github.com/quamarar/seed-job-demo.git')
+                    
                     }
                 }
-            }
-            scriptPath('jenkins/pipelines/job1.Jenkinsfile')
+                         strategy {
+                            defaultBranchPropertyStrategy {
+                               props {
+                                noTriggerBranchProperty()
+                               }
+                            }
+                         }
+                     }
+
+                  }
+configure {
+    it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
+        owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
+        scriptPath('jenkins/pipelines/job1.Jenkinsfile')
+    }
+}
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
         }
     }
 }
