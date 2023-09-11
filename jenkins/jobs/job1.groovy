d213ecc8c41338ee9a12dbd30d6663e00ddea330
quamarar <122767728+quamarar@users.noreply.github.com>
2023-09-11 14:58:08 +0530

    Update job1.groovy

--- jenkins/jobs/job1.groovy
+++ jenkins/jobs/job1.groovy
@@ -4,8 +4,8 @@ multibranchPipelineJob('seed-job-demo/Pipelines/job1') {
 
 parameters {
 
-        choiceParam('ENVIRONMENT', ['dev', 'uat', 'prod'], 'select environment',)
-        chiceParam('action',['plan', 'apply', 'show', 'preview-destroy', 'destroy'], 'Terraform action to apply')
+        choiceParam('ENVIRONMENT', ['dev', 'uat', 'prod'], 'select environment')
+        choiceParam('action', ['plan', 'apply', 'show', 'preview-destroy', 'destroy'], 'Terraform action to apply')
     }
 
     branchSources {
